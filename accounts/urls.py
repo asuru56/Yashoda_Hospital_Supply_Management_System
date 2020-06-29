225a81e5f7814cb09cd3fa650bdd42ff5f9547fd
asuru <65706338+asuru56@users.noreply.github.com>
2020-06-29 14:17:54 +0530

    Update on Customers

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
      path('',views.home,name="home"),
      path('products/',views.products,name='products'),
      path('customer/<str:pk_test>/',views.customer, name="customer"),
-     
+
 ]
+    
